4a8b0c6154b86f18e3aad905ae8869a6bc494fe5
Ricardo Tavares <ricardo.j.f.tavares@gmail.com>
2023-10-03 16:27:12 +0100

    Added missing @Repository annotations

--- Sloppify/src/main/java/Sloppify/Track/SloppifyTrackRepository.java
+++ Sloppify/src/main/java/Sloppify/Track/SloppifyTrackRepository.java
@@ -1,9 +1,9 @@
 package Sloppify.Track;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface SloppifyTrackRepository extends JpaRepository<SloppifyTrack, String> {
 
 }
